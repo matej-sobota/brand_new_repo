dfa62e601f08f20371b7427212d34384bdf068b7
matej-sobota <103167358+matej-sobota@users.noreply.github.com>
2022-08-05 11:27:17 +0200

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,13 +1,2 @@
-pipeline {
-    agent any
-    
-  def _RED='\033[0;31m'  
-  def _END='\033[0m'
-      stages {
-        stage('Example Build') {
-            steps {
-                echo "_RED tota tuja tuna _END"
-            }
-        }
-    }
-}
+def _RED = '\033[0;31m'
+def _END = '\033[0m'
